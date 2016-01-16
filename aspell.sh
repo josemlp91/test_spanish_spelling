42d927fe558c919cb2cada5e47af41305f0d9f9b
@josemlp91 <josemlp.motefrio@gmail.com>
2016-01-16 22:32:42 +0100

    Actualizamos diccionario personal.

--- aspell.sh
+++ aspell.sh
@@ -5,8 +5,17 @@ if  [ -z "$1" ] && [ -z "$2" ]; then
   else
     if [[ $2  = "english" ]];
       then
-        aspell --master=english  -c $1;
+          aspell --master=english  -c $1;
     else
           aspell --master=spanish  -c $1;
   fi
 fi
+
+
+cp  /home/$USER/.aspell.es.pws  ./
+cp  /home/$USER/.aspell.en.pws  ./
+
+git init
+git add .
+git commit -m "Actualizamos diccionario personal."
+git push origin master
